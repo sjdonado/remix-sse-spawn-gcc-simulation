5132af41b8950baf15b02622e81ca51f0acaa444
Juan Rodriguez <sjdonado@icloud.com>
2024-04-21 16:27:26 +0200

    refactor: update chargepoints defaults

--- app/routes/_main._index.tsx
+++ app/routes/_main._index.tsx
@@ -172,7 +172,7 @@ export default function HomePage() {
             name="numChargePoints"
             type="number"
             label="Number of charge points"
-            defaultValue={simulation?.numChargePoints ?? 5}
+            defaultValue={simulation?.numChargePoints ?? 20}
           />
           <Input
             name="arrivalMultiplier"
